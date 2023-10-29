e2627db6c8ff53694fe4be574159e5b3106b089a
Björn Giesler <bjoern@giesler.de>
2023-10-29 15:45:26 +0100

    ooops

--- Arduino/BB8Droid/BB8WifiSecrets.h
+++ Arduino/BB8Droid/BB8WifiSecrets.h
@@ -1,14 +1,8 @@
 #if !defined(BB8WIFISECRETS_H)
 #define BB8WIFISECRETS_H
 
-#if 1
-static const bool  WIFI_AP_MODE  = false;
-static const String WIFI_SSID    = "Hogwarts";
-static const String WIFI_WPA_KEY = "1s(h1pu+Bj0rn";
-#else
 static const bool   WIFI_AP_MODE = true;
 static const String WIFI_SSID    = "BB8Wifi";
 static const String WIFI_WPA_KEY = "BB8WifiKey";
-#endif
 
 #endif
