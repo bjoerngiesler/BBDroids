5833df66221287e1ae33bb89e1fac614bf40940a
Björn Giesler <bjoern@giesler.de>
2024-03-17 21:31:48 +0100

    Removed Unit parameters, use setUnit() instead

---
+++ Arduino/LibBB/include/BBEncoder.h
@@ -0,0 +1,66 @@
+#if !defined(BBENCODER_H)
+#define BBENCODER_H
+
+#if defined(ARDUINO_ARCH_SAMD) // FIXME - get rid of dependency of Arduino Encoder class.
+
+#include <Encoder.h>
+#include <BBControllers.h>
+#include <BBLowPassFilter.h>
+#include <limits.h>
+#include <math.h>
+
+namespace bb {
+class Encoder: public ControlInput {
+public:
+  enum InputMode {
+    INPUT_SPEED    = 0,
+    INPUT_POSITION = 1
+  };
+
+  enum Unit {
+    UNIT_MILLIMETERS = 0,
+    UNIT_TICKS       = 1
+  };
+
+  Encoder(uint8_t pin_enc_a, uint8_t pin_enc_b, InputMode mode = INPUT_POSITION, Unit unit = UNIT_TICKS);
+  
+  void setMode(InputMode mode);
+  void setUnit(Unit unit);
+  InputMode mode() { return mode_; }
+  Unit unit() { return unit_; }
+  virtual float present();
+  virtual float present(InputMode mode, bool raw = false);
+  virtual float presentPosition(bool raw = false);
+  virtual float presentSpeed(bool raw = false);
+  virtual Result update();
+
+  float speedFilterCutoff();
+  void setSpeedFilterCutoff(float co);
+
+  float positionFilterCutoff();
+  void setPositionFilterCutoff(float co);
+
+  void setMillimetersPerTick(float mmPT) { mmPT_ = mmPT; }
+
+  float controlGain() { if(unit_ == UNIT_TICKS) return 1.0f; else return 1/mmPT_; }
+
+protected:
+  InputMode mode_;
+  Unit unit_;
+  ::Encoder enc_; // FIXME -- since this requires SAMD, possibly replace by own encoder handling?
+  bb::LowPassFilter filtSpeed_, filtPos_;
+
+  float mmPT_;
+  long lastCycleTicks_;
+  unsigned long lastCycleUS_;
+  long presentPos_;    // internally always in encoder ticks
+  long presentPosFiltered_;
+  float presentSpeed_; // internally always in encoder ticks per second
+  float presentSpeedFiltered_;
+};
+}
+
+#endif // ARDUINO_ARCH_SAMD
+
+
+#endif //BBENCODER_H
---
+++ Arduino/LibBB/src/BBEncoder.cpp
@@ -0,0 +1,96 @@
+#if defined(ARDUINO_ARCH_SAMD)
+
+#include <BBEncoder.h>
+#include <BBConsole.h>
+
+bb::Encoder::Encoder(uint8_t pin_enc_a, uint8_t pin_enc_b, InputMode mode, Unit unit): 
+  enc_(pin_enc_a, pin_enc_b),
+  filtSpeed_(25, 100, true), 
+  filtPos_(25, 100, true) {
+  mode_ = mode;
+  unit_ = unit;
+  mmPT_ = 1.0;
+  lastCycleUS_ = micros();
+  presentPos_ = enc_.read();
+}
+
+void bb::Encoder::setMode(Encoder::InputMode mode) {
+  mode_ = mode;
+}
+
+void bb::Encoder::setUnit(Encoder::Unit unit) {
+  unit_ = unit;
+}
+
+bb::Result bb::Encoder::update() {
+  unsigned long ticks = enc_.read();
+  lastCycleTicks_ = ticks - presentPos_; // FIXME compensate for wrap?
+  presentPos_ = ticks;
+  presentPosFiltered_ = filtPos_.filter(presentPos_);
+
+  unsigned long us = micros();
+  unsigned long dt;
+  if (us < lastCycleUS_) {
+    dt = (ULONG_MAX - lastCycleUS_) + us;
+  } else {
+    dt = us - lastCycleUS_;
+  }
+  lastCycleUS_ = us;
+
+  presentSpeed_ = ((double)lastCycleTicks_ / (double)dt)*1e6;
+  presentSpeedFiltered_ = filtSpeed_.filter(presentSpeed_);
+
+  return RES_OK;
+}
+
+float bb::Encoder::present(bb::Encoder::InputMode mode, bool raw) {
+  float retval;
+  switch(mode) {
+  case INPUT_SPEED:
+    if(raw) retval = presentSpeed_;
+    else retval = presentSpeedFiltered_;
+    break;
+
+  case INPUT_POSITION:
+  default:
+    if(raw) retval = presentPos_;
+    else retval = presentPosFiltered_;
+    break;
+  }
+
+  if(unit_ == UNIT_TICKS) return retval;
+  else return retval * mmPT_;
+}
+
+  
+float bb::Encoder::present() {
+  return present(mode_);
+}
+
+float bb::Encoder::presentPosition(bool raw) {
+  return present(INPUT_POSITION, raw);
+}
+
+float bb::Encoder::presentSpeed(bool raw) {
+  return present(INPUT_SPEED, raw);  
+}
+
+
+float bb::Encoder::speedFilterCutoff() {
+  return filtSpeed_.cutoff();
+}
+  
+void bb::Encoder::setSpeedFilterCutoff(float co) {
+  filtSpeed_.setCutoff(co);
+}
+
+float  bb::Encoder::positionFilterCutoff() {
+  return filtPos_.cutoff();
+}
+
+void bb::Encoder::setPositionFilterCutoff(float co) {
+  filtPos_.setCutoff(co);
+}
+
+
+#endif // ARDUINO_ARCH_SAMD
