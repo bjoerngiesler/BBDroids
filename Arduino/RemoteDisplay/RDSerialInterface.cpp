988a65721550233051e6af7e50b00956e8c1b7b2
Björn Giesler <bjoern@giesler.de>
2024-08-07 10:37:42 +0200

    Implemented *much* less bandwidth intensive drawing protocol

--- Arduino/RemoteDisplay/RDSerialInterface.cpp
+++ Arduino/RemoteDisplay/RDSerialInterface.cpp
@@ -1,7 +1,25 @@
 #include "RDSerialInterface.h"
 #include "GFX4dIoD9.h"
 
+#include <algorithm>
+
 extern GFX4dIoD9 gfx;
+
+std::map<rd::Command, rd::CmdArgDescription> rd::SerialInterface::cmdArgs = {
+  { CMD_LOGO, { 1, "0 => no backlight, 1 => backlight" } },
+  { CMD_CLS, { 0, "" } },
+  { CMD_POINT, { 3, "x,y,color" } },
+  { CMD_LINE, { 5, "x1,y1,x2,y2,color" } },
+  { CMD_HLINE, { 4, "x,y,width,color" } },
+  { CMD_VLINE, { 4, "x,y,height,color" } },
+  { CMD_RECT, { 5, "x1,y1,x2,y2,color" } },
+  { CMD_FILLEDRECT, { 5, "x1,y1,x2,y2,color" } },
+  { CMD_CIRCLE, { 4, "x,y,radius,color" } },
+  { CMD_FILLEDCIRCLE, { 4, "x,y,radius,color" } },
+  { CMD_TEXT, { 4, "x,y,color,string (ascii: enclosed in "
+                   ", binary: terminated by \0)" } }
+};
+
 
 rd::SerialInterface rd::SerialInterface::serial;
 
@@ -17,292 +35,246 @@ bool rd::SerialInterface::available() {
   return Serial.available();
 }
 
-rd::SerialInterface::Result rd::SerialInterface::handleInput() {
-  if(!Serial.available()) {
-    return RESULT_NOTHING_TO_READ;
-  }
+uint16_t rd::SerialInterface::lookupColor(uint8_t color) {
+  return gfx.RGBto565((color & 0x30) >> 4, (color & 0xC) >> 2, color & 0x03);
+}
 
-	String str;
-  
-  if(readStringUntil('\n', str) == false) return RESULT_ERROR;
+rd::SerialInterface::Result rd::SerialInterface::execText(uint8_t x, uint8_t y, uint8_t color, const String& text) {
+  gfx.MoveTo(x, y);
+  gfx.TextColor(lookupColor(color));
+  gfx.print(text);
+  return RESULT_OK;
+}
 
-	str.trim();
-	std::vector<String> words = split(str);
-
-	if(words.size() == 0) {
-    Serial.println("OK.");
-		return RESULT_OK;
-	}
-
-  if(words[0] == "logo") {
-    if(words.size()!=2) {
-      Serial.println("Usage: logo backlight|nobacklight");
-      return RESULT_ERROR;
-    }
-    if(words[1] == "backlight") {
-      gfx.BacklightOn(true); 
+rd::SerialInterface::Result rd::SerialInterface::readArgsBinary(uint8_t num, std::vector<uint8_t>& args) {
+  args.clear();
+  int timeout = 10;
+  while (timeout > 0) {
+    if (Serial.available()) {
+      args.push_back(Serial.read());
+      if (args.size() == num) return RESULT_OK;
+      timeout = 10;
     } else {
-      gfx.BacklightOn(false);
+      delay(1);
+      timeout--;
     }
-    Serial.println("OK.");
-    return RESULT_RUN_LOGO_SCREEN;
   }
-
-  else if(words[0] == "cls") {
-    gfx.Cls();
-    Serial.println("OK.");
-    return RESULT_OK;
-  }
-
-  else if(words[0] == "print") {
-    if(words.size()!=5) {
-      Serial.println("Usage: print X Y COLOR STRING");
-      return RESULT_ERROR;
-    }
-    int x = words[1].toInt();
-    int y = words[2].toInt();
-    uint16_t color;
-    if(words[3].startsWith("0x")) {
-      color = strtol(words[3].c_str(), NULL, 16);
-    } else {
-      color = words[3].toInt();
-    }
-
-    gfx.MoveTo(x, y);
-    gfx.TextColor(color);
-    gfx.print(words[4]);
-    Serial.println("OK.");
-    return RESULT_OK;
-  }
-
-  else if(words[0] == "hline") {
-    if(words.size()!=5) {
-      Serial.println("Usage: hline X Y WIDTH COLOR");
-      return RESULT_ERROR;
-    }
-    int x = words[1].toInt();
-    int y = words[2].toInt();
-    int width = words[3].toInt();
-    uint16_t color;
-    if(words[4].startsWith("0x")) {
-      color = strtol(words[4].c_str(), NULL, 16);
-    } else {
-      color = words[4].toInt();
-    }
-
-    gfx.Hline(x, y, width, color);
-    Serial.println("OK.");
-    return RESULT_OK;
-  }
-
-  else if(words[0] == "vline") {
-    if(words.size()!=5) {
-      Serial.println("Usage: vline X Y HEIGHT COLOR");
-      return RESULT_ERROR;
-    }
-    int x = words[1].toInt();
-    int y = words[2].toInt();
-    int height = words[3].toInt();
-    uint16_t color;
-    if(words[4].startsWith("0x")) {
-      color = strtol(words[4].c_str(), NULL, 16);
-    } else {
-      color = words[4].toInt();
-    }
-
-    gfx.Vline(x, y, height, color);
-    Serial.println("OK.");
-    return RESULT_OK;
-  }
-
-  else if(words[0] == "line") {
-    if(words.size()!=6) {
-      Serial.println("Usage: line X1 Y1 X2 Y2 COLOR");
-      return RESULT_ERROR;
-    }
-    int x1 = words[1].toInt();
-    int y1 = words[2].toInt();
-    int x2 = words[3].toInt();
-    int y2 = words[4].toInt();
-    uint16_t color;
-    if(words[5].startsWith("0x")) {
-      color = strtol(words[5].c_str(), NULL, 16);
-    } else {
-      color = words[5].toInt();
-    }
-
-    gfx.Line(x1, y1, x2, y2, color);
-    Serial.println("OK.");
-    return RESULT_OK;
-  }
-
-  else if(words[0] == "circle") {
-    if(words.size()!=5) {
-      Serial.println("Usage: circle X Y RADIUS COLOR");
-      return RESULT_ERROR;
-    }
-    int x = words[1].toInt();
-    int y = words[2].toInt();
-    int radius = words[3].toInt();
-    uint16_t color;
-    if(words[4].startsWith("0x")) {
-      color = strtol(words[4].c_str(), NULL, 16);
-    } else {
-      color = words[4].toInt();
-    }
-
-    gfx.Circle(x, y, radius, color);
-    Serial.println("OK.");
-    return RESULT_OK;
-  }
-
-  else if(words[0] == "circlefilled") {
-    if(words.size()!=5) {
-      Serial.println("Usage: circlefilled X Y RADIUS COLOR");
-      return RESULT_ERROR;
-    }
-    int x = words[1].toInt();
-    int y = words[2].toInt();
-    int radius = words[3].toInt();
-    uint16_t color;
-    if(words[4].startsWith("0x")) {
-      color = strtol(words[4].c_str(), NULL, 16);
-    } else {
-      color = words[4].toInt();
-    }
-
-    gfx.CircleFilled(x, y, radius, color);
-    Serial.println("OK.");
-    return RESULT_OK;
-  }
-
-  else if(words[0] == "rect") {
-    if(words.size()!=6) {
-      Serial.println("Usage: rect X1 Y1 x2 y2 COLOR");
-      return RESULT_ERROR;
-    }
-    int x1 = words[1].toInt();
-    int y1 = words[2].toInt();
-    int x2 = words[3].toInt();
-    int y2 = words[4].toInt();
-    uint16_t color;
-    if(words[5].startsWith("0x")) {
-      color = strtol(words[5].c_str(), NULL, 16);
-    } else {
-      color = words[5].toInt();
-    }
-
-    gfx.Rectangle(x1, y1, x2, y2, color);
-    Serial.println("OK.");
-    return RESULT_OK;
-  }
-
-  else if(words[0] == "rectfilled") {
-    if(words.size()!=6) {
-      Serial.println("Usage: rectfilled X1 Y1 x2 y2 COLOR");
-      return RESULT_ERROR;
-    }
-    int x1 = words[1].toInt();
-    int y1 = words[2].toInt();
-    int x2 = words[3].toInt();
-    int y2 = words[4].toInt();
-    uint16_t color;
-    if(words[5].startsWith("0x")) {
-      color = strtol(words[5].c_str(), NULL, 16);
-    } else {
-      color = words[5].toInt();
-    }
-
-    gfx.RectangleFilled(x1, y1, x2, y2, color);
-    Serial.println("OK.");
-    return RESULT_OK;
-  }
-
-  else if(words[0] == "backlight") {
-    if(words.size()!=2) {
-      Serial.println("Usage: backlight on|off");
-      return RESULT_ERROR;
-    }
-    if(words[1] == "on") gfx.BacklightOn(true);
-    else gfx.BacklightOn(false);
-    Serial.println("OK.");
-    return RESULT_OK;
-  }
-
-  else if(words[0] == "scroll") {
-    if(words.size()!=2) {
-      Serial.println("Usage: scroll NUMPIXELS");
-      return RESULT_ERROR;
-    }
-    int numpixels = words[1].toInt();
-    gfx.Scroll(numpixels);
-    Serial.println("OK.");
-    return RESULT_OK;
-  }
-
-  else Serial.println(String("Unknown command \"") + words[0] + "\".");
   return RESULT_ERROR;
 }
 
-bool rd::SerialInterface::readStringUntil(unsigned char c, String& str) { 
-	unsigned char input = Serial.read();
-
-	if(input == '\b') {
-		if(curStr_.length() > 0) curStr_.remove(curStr_.length()-1);
-	} else {
-		curStr_ += (char)input;
-	}
-
-	Serial.flush();
-	str = curStr_;
-	if(input == c) {
-		curStr_ = "";
-		return true;
-	}
-
-	return false;
+rd::SerialInterface::Result rd::SerialInterface::readStringUntil(uint8_t delim, String& str) {
+  int timeout = 10;
+  while (timeout > 0) {
+    if (Serial.available()) {
+      uint8_t c = Serial.read();
+      if (c == delim) return RESULT_OK;
+      str += c;
+      timeout = 10;
+    } else {
+      delay(1);
+      timeout--;
+    }
+  }
+  return RESULT_ERROR;
 }
 
-std::vector<String> rd::SerialInterface::split(const String& str) {
+rd::SerialInterface::Result rd::SerialInterface::execInput(Command cmd, std::vector<uint8_t> args) {
+  // Double sanity check input; this has been checked before calling, but just to make sure.
+  if (cmdArgs.find(cmd) == cmdArgs.end()) return RESULT_ERROR;
+  uint8_t numArgs = cmdArgs[cmd].numArgs;
+  if (args.size() != numArgs) return RESULT_ERROR;
+
+  switch (cmd) {
+    case CMD_LOGO:
+      gfx.BacklightOn(args[0]);
+      return RESULT_RUN_LOGO_SCREEN;
+
+    case CMD_CLS:
+      gfx.Cls();
+      return RESULT_OK;
+
+    case CMD_HLINE:
+      gfx.Hline(args[0], args[1], args[2], lookupColor(args[3]));
+      return RESULT_OK;
+
+    case CMD_VLINE:
+      gfx.Hline(args[0], args[1], args[2], lookupColor(args[3]));
+      return RESULT_OK;
+
+    case CMD_LINE:
+      gfx.Line(args[0], args[1], args[2], args[3], lookupColor(args[4]));
+      return RESULT_OK;
+
+    case CMD_CIRCLE:
+      gfx.Circle(args[0], args[1], args[2], lookupColor(args[3]));
+      return RESULT_OK;
+
+    case CMD_FILLEDCIRCLE:
+      gfx.CircleFilled(args[0], args[1], args[2], lookupColor(args[3]));
+      return RESULT_OK;
+
+    case CMD_RECT:
+      gfx.Rectangle(args[0], args[1], args[2], args[3], lookupColor(args[4]));
+      return RESULT_OK;
+
+    case CMD_FILLEDRECT:
+      gfx.RectangleFilled(args[0], args[1], args[2], args[3], lookupColor(args[4]));
+      return RESULT_OK;
+
+    default:
+      break;
+  }
+
+  return RESULT_ERROR;
+}
+
+rd::SerialInterface::Result rd::SerialInterface::handleInput() {
+  if (!Serial.available()) {
+    return RESULT_NOTHING_TO_READ;
+  }
+
+  uint8_t cmdByte = (Command)Serial.read();
+  Command cmd;
+  std::vector<uint8_t> args;
+  bool binary = false;
+
+  if (cmdByte & 0x80) {  // binary; strip high bit and continue
+    cmdByte &= 0x7f;
+    binary = true;
+  }
+
+  cmd = (Command)cmdByte;
+
+  // NOPs get a direct "OK" reply
+  if (cmd == CMD_NOP) {
+    if (binary) Serial.write(CMD_NOP | 0x80);
+    else Serial.println("OK.");
+    return RESULT_OK;
+  }
+
+  // Lookup number of arguments and help message
+  if (cmdArgs.find(cmd) == cmdArgs.end()) {
+    if (binary) Serial.write(CMD_ERROR | 0x80);
+    else Serial.println(String("Unknown command \'") + String(cmdByte) + "\'.");
+    return RESULT_ERROR;
+  }
+
+  uint8_t numArgs = cmdArgs[cmd].numArgs;
+  const char* helpMessage = cmdArgs[cmd].helpMessage;
+
+  if (binary) {
+
+    // Text command handled separately because of the variable argument list
+    if (cmd == CMD_TEXT) {
+      if (readArgsBinary(3, args) != RESULT_OK) {
+        Serial.write(CMD_ERROR | 0x80);
+        return RESULT_ERROR;
+      }
+
+      String text;
+      if (readStringUntil('\0', text) != RESULT_OK) {
+        Serial.write(CMD_ERROR | 0x80);
+        return RESULT_ERROR;
+      }
+
+      if(execText(args[0], args[1], args[2], text) != RESULT_OK) {
+        Serial.write(CMD_ERROR | 0x80);
+        return RESULT_ERROR;
+      }
+
+      return RESULT_OK;
+    }
+
+    // Still here? Read arguments and continue
+    if(readArgsBinary(numArgs, args) != RESULT_OK) {
+      Serial.write(CMD_ERROR | 0x80);
+      return RESULT_ERROR;
+    }
+
+  } else {  // ASCII
+
+    // Read line and split into words
+    String str;
+    if (readStringUntil('\n', str) != RESULT_OK) {
+      return RESULT_ERROR;
+    }
+    str.trim();
+    std::vector<String> words = split(str);
+
+    // Text again handled separately
+    if (cmd == CMD_TEXT) {
+      if (words.size() != numArgs) {
+        Serial.println(String("Usage: ") + String(cmdByte) + helpMessage);
+        return RESULT_ERROR;
+      }
+      if (execText(words[0].toInt(), words[1].toInt(), words[2].toInt(), words[3]) != RESULT_OK) {
+        Serial.println("Error handling text!");
+        return RESULT_ERROR;
+      }
+      return RESULT_OK;
+    }
+
+    // Still here? Convert arguments and continue
+    for (auto w : words) args.push_back(w.toInt());
+  }
+
+  // Double check correct number of arguments
+  if (args.size() != numArgs) {
+    if (binary) Serial.write(CMD_ERROR | 0x80);
+    else Serial.println(String("Usage: ") + String(cmdByte) + helpMessage);
+    return RESULT_ERROR;
+  }
+
+  // ...and execute.
+  rd::SerialInterface::Result res = execInput(cmd, args);
+  if (res == RESULT_ERROR) {
+    if (binary) Serial.write(CMD_ERROR | 0x80);
+    else Serial.println("Error.");
+  }
+
+  return res;
+}
+
+std::vector<String> rd::SerialInterface::split(const String& str, unsigned char delim) {
   std::vector<String> words;
   unsigned int start = 0, end = 0;
   bool quotes = false;
 
-  while(end < str.length()) {
-    if(quotes == true) {
-      if(str[end] != '"') {
+  while (end < str.length()) {
+    if (quotes == true) {
+      if (str[end] != '"') {
         end++;
-        if(end == str.length()) {
+        if (end == str.length()) {
           String substr = str.substring(start, end);
-          if(substr.length()) words.push_back(substr);
+          if (substr.length()) words.push_back(substr);
         }
-      } else if(end >= start) {
+      } else if (end >= start) {
         quotes = false;
         String substr = str.substring(start, end);
-        if(substr.length()) words.push_back(substr);
-        start = end+1;
-        end = end+1;
+        if (substr.length()) words.push_back(substr);
+        start = end + 1;
+        end = end + 1;
       }
     } else {
-      if(str[end] == '"') {
+      if (str[end] == '"') {
         quotes = true;
         start++;
         end++;
         continue;
       }
-  
-      if(str[end] != ' ') {
+
+      if (str[end] != delim) {
         end++;
-        if(end == str.length()) {
+        if (end == str.length()) {
           String substr = str.substring(start, end);
-          if(substr.length()) words.push_back(substr);
+          if (substr.length()) words.push_back(substr);
         }
-      } else if(end >= start) {
+      } else if (end >= start) {
         String substr = str.substring(start, end);
-        if(substr.length()) words.push_back(substr);
-        start = end+1;
-        end = end+1;
-      } 
+        if (substr.length()) words.push_back(substr);
+        start = end + 1;
+        end = end + 1;
+      }
     }
   }
 
--- Arduino/RemoteDisplay/RDSerialInterface.h
+++ Arduino/RemoteDisplay/RDSerialInterface.h
@@ -1,7 +1,29 @@
 #include <Arduino.h>
 #include <vector>
+#include <map>
 
 namespace rd {
+
+struct CmdArgDescription {
+  uint8_t numArgs;
+  const char* helpMessage;
+};
+
+enum Command {
+  CMD_NOP          = '\n',
+  CMD_ERROR        = 'E',
+  CMD_LOGO         = 'L',
+  CMD_CLS          = 'x',
+  CMD_POINT        = 'p',
+  CMD_LINE         = 'l',
+  CMD_HLINE        = 'h',
+  CMD_VLINE        = 'v',
+  CMD_RECT         = 'r',
+  CMD_FILLEDRECT   = 'R',
+  CMD_CIRCLE       = 'c',
+  CMD_FILLEDCIRCLE = 'C',
+  CMD_TEXT         = 't'
+};
 
 class SerialInterface {
 public:
@@ -13,17 +35,21 @@ public:
   } Result;
 
   static SerialInterface serial;
+  static std::map<Command, CmdArgDescription> cmdArgs;
 
   bool begin();
   Result handleInput();
+  Result execInput(Command cmd, const std::vector<uint8_t> args);
+  Result execText(uint8_t x, uint8_t y, uint8_t color, const String& text);
+
   bool available();
 
 protected:
-  bool readStringUntil(unsigned char c, String& str);
-  std::vector<String> split(const String& str);
+  uint16_t lookupColor(uint8_t color);
+  Result readArgsBinary(uint8_t num, std::vector<uint8_t>& args);
+  Result readStringUntil(unsigned char c, String& str);
+  std::vector<String> split(const String& str, unsigned char delim=' ');
   SerialInterface();
-
-  String curStr_;
 };
 
 };
