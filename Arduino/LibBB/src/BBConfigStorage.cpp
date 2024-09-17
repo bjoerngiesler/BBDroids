4b2afd2b9f1924741140e169cdd00536b5e20678
Björn Giesler <bjoern@giesler.de>
2024-09-17 23:02:56 +0200

    Couple bugs in Config Storage fixed, seems to be working now. Reset not yet implemented.

--- Arduino/LibBB/src/BBConfigStorage.cpp
+++ Arduino/LibBB/src/BBConfigStorage.cpp
@@ -30,12 +30,17 @@ bb::ConfigStorage::HANDLE bb::ConfigStorage::reserveBlock(const char* name, size
 
 #if defined(ARDUINO_ARCH_ESP32)
 	nvs_handle_t handle;
-	nvs_open(name, NVS_READWRITE, &handle);
-#endif
+	esp_err_t err = nvs_open(name, NVS_READWRITE, &handle);
+	if(err != ESP_OK) {
+		Serial.println(String("nvs_open() returned ") + err);
+		return RES_CONFIG_INVALID_HANDLE;
+	}
+	Block block = {handle, size};
+#else
 	Block block = {nextHandle_+1, size};
-	blocks_.push_back(block);
 	nextHandle_ = nextHandle_ + size + 1;
-
+#endif
+	blocks_.push_back(block);
 	return block.handle;
 }
 
@@ -47,8 +52,26 @@ bb::Result bb::ConfigStorage::writeBlock(HANDLE handle, uint8_t* data) {
   			size_t size = block.size;
   			Serial.print("Storing "); Serial.print(size); Serial.print(" bytes of data at address "); Serial.println(handle, HEX);
 #if defined(ARDUINO_ARCH_ESP32)
-			nvs_set_blob(handle, "blob", data, size);
-			nvs_set_i8(handle, "valid", 1);
+			esp_err_t err;
+			err = nvs_set_blob(handle, "blob", data, size);
+			if(err != ESP_OK) {
+				Serial.println(String("nvs_set_blob() returned ") + err);
+				return RES_CONFIG_INVALID_HANDLE;
+			}
+			
+			err = nvs_set_i8(handle, "valid", 1);
+			if(err != ESP_OK) {
+				Serial.println(String("nvs_set_i8() returned ") + err);
+				return RES_CONFIG_INVALID_HANDLE;
+			}
+
+			err = nvs_commit(handle);
+			if(err != ESP_OK) {
+				Serial.println(String("nvs_commit() returned ") + err);
+				return RES_CONFIG_INVALID_HANDLE;
+			}
+
+  			Serial.println("Commited blob.");
 #else
   			for(size_t i=0; i<size; i++) {
   				Serial.print(handle+i);
@@ -67,11 +90,14 @@ bb::Result bb::ConfigStorage::writeBlock(HANDLE handle, uint8_t* data) {
 
 bb::Result bb::ConfigStorage::readBlock(HANDLE handle, uint8_t* data) {
 	if(!initialized_) return RES_CONFIG_INVALID_HANDLE;
-	return RES_CONFIG_INVALID_HANDLE;
 	for(auto block: blocks_) {
   		if(block.handle == handle) {
 #if defined(ARDUINO_ARCH_ESP32)
-			nvs_get_blob(handle, "blob", data, &block.size);
+			esp_err_t err = nvs_get_blob(handle, "blob", data, &block.size);
+			if(err != ESP_OK) {
+				Serial.println(String("nvs_get_blob() returned ") + err);
+				return RES_CONFIG_INVALID_HANDLE;
+			}
 #else
   			size_t size = block.size;
   			for(size_t i=0; i<size; i++) {
@@ -91,8 +117,8 @@ bool bb::ConfigStorage::blockIsValid(HANDLE handle) {
   		if(block.handle == handle) {
 #if defined(ARDUINO_ARCH_ESP32)
 			int8_t i8 = 0;
-			nvs_get_i8(handle, "valid", &i8);
-			if(i8 == 1) return true;
+			esp_err_t err = nvs_get_i8(handle, "valid", &i8);
+			if(err == ESP_OK && i8 == 1) return true;
 #else
   			if(EEPROM.read(handle-1) == 0xba) return true;
 #endif
@@ -108,7 +134,12 @@ bool bb::ConfigStorage::initialize() {
 #if defined(ARDUINO_NANO_RP2040_CONNECT)
 	EEPROM.begin(MAXSIZE);
 #elif defined(ARDUINO_ARCH_ESP32)
-	nvs_flash_init();
+	esp_err_t err = nvs_flash_init();
+	if(err != ESP_OK) {
+		Serial.println(String("Error initializing flash: ") + err);
+		return false;
+	}
+	Serial.println("Flash initialized OK.\n");
 #endif
 	maxSize_ = MAXSIZE;
 
