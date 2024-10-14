0c963d4a289688254884d3fccb1439f083758af3
Björn Giesler <bjoern@giesler.de>
2024-10-14 11:39:57 +0200

    Whatever

--- Arduino/Utilities/XBeeTransparent/XBeeTransparent.ino
+++ Arduino/Utilities/XBeeTransparent/XBeeTransparent.ino
@@ -2,7 +2,8 @@
 #include <wiring_private.h>
 
 #define FULLY_TRANSPARENT
-//#define DROID 
+#define DROID
+#define BAUD 9600
 
 #ifdef DROID
 static const uint8_t P_SERIALTX_TX = 0;
@@ -17,9 +18,9 @@ HardwareSerial *serialTXSerial;
 
 void setup(void) {
 #ifdef FULLY_TRANSPARENT
-  Serial.begin(9600);
+  Serial.begin(BAUD);
 #else
-  Serial.begin(115200);
+  Serial.begin(BAUD);
   while(!Serial);
   Serial.println();
   Serial.println("Enter '+++' for AT mode. Use Carriage Return as line end.");
@@ -30,10 +31,10 @@ void setup(void) {
   pinPeripheral(P_SERIALTX_RX, PIO_SERCOM);
   pinPeripheral(P_SERIALTX_TX, PIO_SERCOM);
 #else // Remote
-  serialTXSerial = &Serial1;
-#endif
+   serialTXSerial = &Serial1;
+ #endif
 
-  serialTXSerial->begin(9600);
+  serialTXSerial->begin(BAUD);
 }
 
 void loop(void) {
