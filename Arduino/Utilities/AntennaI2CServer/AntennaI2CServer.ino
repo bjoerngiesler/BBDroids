b69c7788d4ae17ef277784401ca9b1569a070dca
Björn Giesler <bjoern@giesler.de>
2024-11-13 14:54:22 +0100

    Adapted to QT Py ESP32S2

--- Arduino/Utilities/AntennaI2CServer/AntennaI2CServer.ino
+++ Arduino/Utilities/AntennaI2CServer/AntennaI2CServer.ino
@@ -1,16 +1,23 @@
 #include <Wire.h>
+#include <Servo.h>
+#include <Adafruit_NeoPixel.h>
 
 static const uint8_t I2C_ADDRESS  = 0x17;
 
-static const uint8_t SERVO_MIN = 130;
-static const uint8_t SERVO_MAX = 255;
+static const uint8_t SERVO_MIN = 0;
+static const uint8_t SERVO_MAX = 180;
 static const uint8_t SERVO_CENTER = (SERVO_MAX-SERVO_MIN)/2 + SERVO_MIN;
 
-int servoPins[3] = {9, 10, 11};
+int servoPins[3] = {35, 36, 37};
+Servo servos[3];
 int servoSetpoints[3] = {SERVO_CENTER, SERVO_CENTER, SERVO_CENTER};
 int servoCurrents[3] = {SERVO_CENTER, SERVO_CENTER, SERVO_CENTER};
 uint8_t servoSpeed = 1;
 uint8_t cur_reg = 0;
+
+Adafruit_NeoPixel pixels(1, PIN_NEOPIXEL, NEO_GRB + NEO_KHZ800);
+
+#define WIRE Wire1
 
 void writeServos(bool hard) {
   for(int i=0; i<3; i++) {
@@ -28,35 +35,46 @@ void writeServos(bool hard) {
         }
       }
     }
-    analogWrite(servoPins[i], servoCurrents[i]);
+    servos[i].write(servoPins[i], float(servoCurrents[i]));
   }
 }
 
 void receiveEvent(int howmany) {
-  if(!Wire.available()) return;
+  if(!WIRE.available()) return;
   if(howmany == 3) {
     for(int i=0; i<3; i++) {
-      servoSetpoints[i] = map(Wire.read(), 0, 255, SERVO_MIN, SERVO_MAX);
+      servoSetpoints[i] = map(Wire.read(), 0, 180, SERVO_MIN, SERVO_MAX);
     }
   }
 }
 
 void requestEvent() {
   for(uint8_t i=0; i<3; i++) {
-    Wire.write(map(servoSetpoints[i], SERVO_MIN, SERVO_MAX, 0, 255));
+    WIRE.write(map(servoSetpoints[i], SERVO_MIN, SERVO_MAX, 0, 180));
   }
 }
 
 void setup() {
-  writeServos(true);
+#if defined(NEOPIXEL_POWER)
+  pinMode(NEOPIXEL_POWER, OUTPUT);
+  digitalWrite(NEOPIXEL_POWER, HIGH);
+  pixels.begin();
+  pixels.setBrightness(20);
+  pixels.fill(0x00ff00);
+  pixels.show();
+#endif
+
   Serial.begin(115200);
   Serial.println("Antenna Servos I2C Slave");
   Serial.println("2024 by Bjoern Giesler");
   Serial.println(String("Listening on 0x") + String(I2C_ADDRESS, HEX));
   Serial.println("Listening to i2c...");
-  Wire.begin(I2C_ADDRESS);
-  Wire.onReceive(receiveEvent);
-  Wire.onRequest(requestEvent);
+
+  for(int i=0; i<3; i++) servos[i].attach(servoPins[i]);
+  writeServos(true);
+  WIRE.begin(I2C_ADDRESS, SDA1, SCL1, 100000);
+  WIRE.onReceive(receiveEvent);
+  WIRE.onRequest(requestEvent);
 }
 
 void loop() {
@@ -65,7 +83,7 @@ void loop() {
     uint8_t pos = str.toInt();
     Serial.println(String("Setting to ") + pos);
     for(int i=0; i<3; i++) {
-      servoSetpoints[i] = map(pos, 0, 255, SERVO_MIN, SERVO_MAX);
+      servoSetpoints[i] = map(pos, 0, 180, SERVO_MIN, SERVO_MAX);
     }
   }
 
