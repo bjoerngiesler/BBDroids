058faf2b757ad66be8fe474bb90c26b8ba57e5f3
Björn Giesler <bjorngiesler@Air-von-Bjorn.fritz.box>
2025-01-12 17:28:46 +0100

    Debug messages pruned

--- Arduino/Remote/src/RMessageWidget.cpp
+++ Arduino/Remote/src/RMessageWidget.cpp
@@ -7,8 +7,4 @@ void RMessageWidget::setTitle(const String& title) {
   setDrawsFrame();
   RLabelWidget::setTitle(title);
   centerOnMain();
-  Console::console.printfBroadcast("%d lines\n", lines_.size());
-  for(unsigned int i=0; i<lines_.size(); i++) {
-    Console::console.printfBroadcast("%d: '%s'\n", i, lines_[i].c_str());
-  }
 }
