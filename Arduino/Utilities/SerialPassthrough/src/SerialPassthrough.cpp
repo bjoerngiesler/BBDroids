e059e5c5778dc6ccf8b5376a3dbb6757f97c43e4
Björn Giesler <bjoern@giesler.de>
2025-02-17 18:20:02 +0100

    works

--- Arduino/Utilities/SerialPassthrough/src/SerialPassthrough.cpp
+++ Arduino/Utilities/SerialPassthrough/src/SerialPassthrough.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <vector>
 #include <algorithm>
+#include <LibBB.h>
 #include <wiring_private.h>
 
 std::vector<int> standardBaudRates = {9600, 19200, 28800, 38400, 57600, 76800, 115200};
@@ -16,29 +17,63 @@ void SERCOM3_Handler() {
 void setup() {
   Serial.begin(2000000);
   while(!Serial);
+  
+  pinPeripheral(P_PASSTHROUGH_RX, PIO_SERCOM);
+  pinPeripheral(P_PASSTHROUGH_TX, PIO_SERCOM);
+
+  Serial.println();
   Serial.println("SerialPassthrough");
   Serial.println("=================");
-  Serial.println("This program will pass through all characters typed on the console serial port to the");
-  Serial.println(String("serial port at RX=") + P_PASSTHROUGH_RX + ", TX=" + P_PASSTHROUGH_TX + "and vice versa.");
-  
+  Serial.println("This program will pass through all characters typed on the console serial port");
+  Serial.println(String("to the serial port at RX=") + P_PASSTHROUGH_RX + ", TX=" + P_PASSTHROUGH_TX + " and vice versa.");
+  Serial.println("Does not echo; turn local echo on in your terminal if required.");
+}
+
+void begin() {
+  int baud;
+
+  Serial.println();
   while(true) {
-    Serial.print("Please enter baudrate to open the port at: ");
-    String answer = Serial.readStringUntil('\n');
-    int baud = answer.toInt();
+    String answer;
+    Serial.println("Please enter baudrate to use:");
+    Serial.print("> ");
+    while(bb::SerialConsoleStream::readStringUntil(Serial, '\n', answer) == false);
+
+    baud = answer.toInt();
     if(baud <= 0) {
       Serial.println("Error: Baud must be larger than zero.");
       continue;
     }
-    if(std::find(standardBaudRates.begin(), standardBaudRates.end(), baud) == standardBaudRates.end()) {
-      Serial.println("Nonstandard baud rate, trying anyway.");
-    }
-
-    passthroughSerial.begin(baud);
     break;
   }
+
+  if(std::find(standardBaudRates.begin(), standardBaudRates.end(), baud) == standardBaudRates.end()) {
+    Serial.println("Nonstandard baud rate, trying anyway.");
+  }
+
+  Serial.println("Passing all characters through from now on. Type ESC to exit.");
+  passthroughSerial.begin(baud);
 }
 
 void loop() {
-  while(Serial.available()) passthroughSerial.write(Serial.read());
-  while(passthroughSerial.available()) Serial.write(passthroughSerial.read());
+  begin();
+  while(true) {
+    uint8_t c;
+
+    while(passthroughSerial.available()) {
+      c = passthroughSerial.read();
+      Serial.write(c);
+      if(c == '\r') Serial.write('\n');
+    }
+
+    while(Serial.available()) {
+      uint8_t c = Serial.read();
+      if(c == 0x1b) { // escape
+        Serial.println();
+        Serial.println("ESC caught, exiting.");
+        return;
+      }
+      passthroughSerial.write(c);
+    }
+  }
 }
